819daf6dfd7ef2f73b6aaeca242692b1acb937c0
kay.stenschke <stenschke@gyselroth.com>
2020-04-08 12:45:37 +0200

    cleanup

--- src/docxbox/main.cpp
+++ src/docxbox/main.cpp
@@ -6,10 +6,6 @@
  * @param argv Array of arguments received, argv[0] is name and path of executable
  */
 int main(int argc, char **argv) {
-  //argc = 3;
-  //argv[1] = "txt";
-  //argv[2] = "/home/kay/CLionProjects/docxengine/test/files/file-sample_100kB.docx";
-
   // Process command arguments, display help if no valid command given
   auto *app = new docxbox::App(argc, argv);
 
