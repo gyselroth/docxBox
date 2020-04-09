a16dfc709d4dcdb1e5d86b94619e2f09e4f7dda1
LucasBornhauser <bornhauser@gyselroth.com>
2020-04-09 15:47:58 +0200

    added missing argument testCase

--- test/functional/rpt.bats.sh
+++ test/functional/rpt.bats.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bats
 
 load _helper
+
+@test "Output of \"docxbox rpt {missing argument}\" is an error message" {
+  run $BATS_TEST_DIRNAME/docxbox rpt
+  [ "$status" -ne 0 ]
+  [ "Missing argument: Filename of DOCX to be extracted" = "${lines[0]}" ]
+}
