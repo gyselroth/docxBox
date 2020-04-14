8c790f70590a390524e2afb29d688d9abf14ae99
LucasBornhauser <bornhauser@gyselroth.com>
2020-04-14 10:54:02 +0200

    finished implementing testSuite lsij.bats.sh

--- test/functional/lsij.bats.sh
+++ test/functional/lsij.bats.sh
@@ -2,8 +2,17 @@
 
 load _helper
 
+@test "Exit code of \"docxbox lsij filename.docx\" is zero" {
+  run $BATS_TEST_DIRNAME/docxbox lsij test/files/table_unordered_list_images.docx
+  [ "$status" -eq 0 ]
+}
+
 @test "Output of \"docxbox lsij {missing argument}\" is an error message" {
   run "$BATS_TEST_DIRNAME"/docxbox lsij
   [ "$status" -ne 0 ]
   [ "Missing argument: Filename of DOCX to be extracted" = "${lines[0]}" ]
+}
+
+@test "Output of \"docxbox lsi filename.docx\" is contained images" {
+  $BATS_TEST_DIRNAME/docxbox lsi test/files/table_unordered_list_images.docx | grep -c "image1.jpeg"
 }
