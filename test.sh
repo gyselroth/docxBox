4b184ea91386895fc292a0c6d8df57a9d310108b
LucasBornhauser <bornhauser@gyselroth.com>
2020-04-21 13:25:59 +0200

    added newly implemented testSuite to test.sh +1

--- test.sh
+++ test.sh
@@ -9,71 +9,71 @@
 
 START_TIME=$SECONDS
 
-#printf "\033[4mTest display of help\033[0m\n"
-#bats ./test/functional/help.bats.sh
-#
-#printf "\n\033[4mTest display of version number\033[0m\n"
-#bats ./test/functional/version.bats.sh
-#
-#printf "\n\033[4mTest listing files in DOCX\033[0m\n"
-#bats ./test/functional/ls.bats.sh
-#
-#printf "\n\033[4mTest listing files in DOCX as JSON\033[0m\n"
-#bats ./test/functional/lsj.bats.sh
-#
-#printf "\n\033[4mTest listing fields in DOCX\033[0m\n"
-#bats ./test/functional/lsd.bats.sh
-#
-#printf "\n\033[4mTest listing fields in DOCX as JSON\033[0m\n"
-#bats ./test/functional/lsdj.bats.sh
-#
-#printf "\n\033[4mTest listing referenced fonts in DOCX\033[0m\n"
-#bats ./test/functional/lsf.bats.sh
-#
-#printf "\n\033[4mTest listing referenced fonts in DOCX as JSON\033[0m\n"
-#bats ./test/functional/lsfj.bats.sh
-#
-#printf "\n\033[4mTest listing merge fields in DOCX\033[0m\n"
-#bats ./test/functional/lsg.bats.sh
-#
-#printf "\n\033[4mTest listing merge fields in DOCX as JSON\033[0m\n"
-#bats ./test/functional/lsgj.bats.sh
-#
-#printf "\n\033[4mTest listing images in DOCX\033[0m\n"
-#bats ./test/functional/lsi.bats.sh
-#
-#printf "\n\033[4mTest listing images in DOCX as JSON\033[0m\n"
-#bats ./test/functional/lsij.bats.sh
-#
-#printf "\n\033[4mTest listing metadata in DOCX\033[0m\n"
-#bats ./test/functional/lsm.bats.sh
-#
-#printf "\n\033[4mTest listing metadata in DOCX as JSON\033[0m\n"
-#bats ./test/functional/lsmj.bats.sh
-#
-#printf "\n\033[4mTest replacing image in DOCX\033[0m\n"
-#bats ./test/functional/rpi.bats.sh
-#
-#printf "\n\033[4mTest replacing text in DOCX\033[0m\n"
-#bats ./test/functional/rpt.bats.sh
-#
-#printf "\n\033[4mTest modyfing or setting meta attributes in DOCX\033[0m\n"
-#bats ./test/functional/mm.bats.sh
+printf "\033[4mTest display of help\033[0m\n"
+bats ./test/functional/help.bats.sh
+
+printf "\n\033[4mTest display of version number\033[0m\n"
+bats ./test/functional/version.bats.sh
+
+printf "\n\033[4mTest listing files in DOCX\033[0m\n"
+bats ./test/functional/ls.bats.sh
+
+printf "\n\033[4mTest listing files in DOCX as JSON\033[0m\n"
+bats ./test/functional/lsj.bats.sh
+
+printf "\n\033[4mTest listing fields in DOCX\033[0m\n"
+bats ./test/functional/lsd.bats.sh
+
+printf "\n\033[4mTest listing fields in DOCX as JSON\033[0m\n"
+bats ./test/functional/lsdj.bats.sh
+
+printf "\n\033[4mTest listing referenced fonts in DOCX\033[0m\n"
+bats ./test/functional/lsf.bats.sh
+
+printf "\n\033[4mTest listing referenced fonts in DOCX as JSON\033[0m\n"
+bats ./test/functional/lsfj.bats.sh
+
+printf "\n\033[4mTest listing merge fields in DOCX\033[0m\n"
+bats ./test/functional/lsg.bats.sh
+
+printf "\n\033[4mTest listing merge fields in DOCX as JSON\033[0m\n"
+bats ./test/functional/lsgj.bats.sh
+
+printf "\n\033[4mTest listing images in DOCX\033[0m\n"
+bats ./test/functional/lsi.bats.sh
+
+printf "\n\033[4mTest listing images in DOCX as JSON\033[0m\n"
+bats ./test/functional/lsij.bats.sh
+
+printf "\n\033[4mTest listing metadata in DOCX\033[0m\n"
+bats ./test/functional/lsm.bats.sh
+
+printf "\n\033[4mTest listing metadata in DOCX as JSON\033[0m\n"
+bats ./test/functional/lsmj.bats.sh
+
+printf "\n\033[4mTest replacing image in DOCX\033[0m\n"
+bats ./test/functional/rpi.bats.sh
+
+printf "\n\033[4mTest replacing text in DOCX\033[0m\n"
+bats ./test/functional/rpt.bats.sh
+
+printf "\n\033[4mTest modyfing or setting meta attributes in DOCX\033[0m\n"
+bats ./test/functional/mm.bats.sh
 
 printf "\n\033[4mTest replacing text with dummy text in DOCX\033[0m\n"
 bats ./test/functional/lorem.bats.sh
 
-#printf "\n\033[4mTest output DOCX document as plaintext\033[0m\n"
-#bats ./test/functional/txt.bats.sh
-#
-#printf "\n\033[4mTest unziping files from DOCX\033[0m\n"
-#bats ./test/functional/uz.bats.sh
-#
-#printf "\n\033[4mTest unziping only media files from DOCX\033[0m\n"
-#bats ./test/functional/uzm.bats.sh
-#
-#printf "\n\033[4mTest creating (zip) DOCX from files\033[0m\n"
-#bats ./test/functional/zip.bats.sh
+printf "\n\033[4mTest output DOCX document as plaintext\033[0m\n"
+bats ./test/functional/txt.bats.sh
+
+printf "\n\033[4mTest unziping files from DOCX\033[0m\n"
+bats ./test/functional/uz.bats.sh
+
+printf "\n\033[4mTest unziping only media files from DOCX\033[0m\n"
+bats ./test/functional/uzm.bats.sh
+
+printf "\n\033[4mTest creating (zip) DOCX from files\033[0m\n"
+bats ./test/functional/zip.bats.sh
 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 printf "\nDone. Bats tests ran for $ELAPSED_TIME seconds.\n\n";
