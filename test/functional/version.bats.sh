6342a5d0e1566d7c2eab567f5ea414b038b9d7ee
kay.stenschke <stenschke@gyselroth.com>
2020-04-23 10:06:44 +0200

    follow code conventions

--- test/functional/version.bats.sh
+++ test/functional/version.bats.sh
@@ -3,6 +3,6 @@
 load _helper
 
 @test "\"docxbox v\" displays version number" {
-  #todo: check needs to be more dynamic
+  # TODO(lucas): check needs to be more dynamic
   "$BATS_TEST_DIRNAME"/docxbox v | grep '0.0.2'
 }
