71679de2d9983320ff22abff2536647da1372fbd
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-05 10:22:30 +0200

    fixed failing testCases

--- test/functional/ls.bats.sh
+++ test/functional/ls.bats.sh
@@ -40,6 +40,6 @@ load _helper
 }
 
 @test "Output of \"docxbox ls filename.docx\" contains files' date and time" {
-  "$BATS_TEST_DIRNAME"/docxbox ls test/functional/tmp/cp_table_unordered_list_images.docx | grep -c "04/11/2020"
-  "$BATS_TEST_DIRNAME"/docxbox ls test/functional/tmp/cp_table_unordered_list_images.docx | grep -c "11:03"
+  "$BATS_TEST_DIRNAME"/docxbox ls test/functional/tmp/cp_table_unordered_list_images.docx | grep -c "4/11/2020"
+  "$BATS_TEST_DIRNAME"/docxbox ls test/functional/tmp/cp_table_unordered_list_images.docx | grep -c "11:3"
 }
