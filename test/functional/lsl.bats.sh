3e3ea7006497b5f0b2d61b598275052558c1bfdb
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-06 12:27:29 +0200

    extended lsl.bats.sh - tests lsl command with regex

--- test/functional/lsl.bats.sh
+++ test/functional/lsl.bats.sh
@@ -13,16 +13,41 @@ load _helper
   [ "Missing argument: DOCX filename" = "${lines[0]}" ]
 }
 
-# @TODO: throws an error: related github issue: #22
-#@test "Output of \"docxbox lsl filename.docx {missing argument}\" is an error message" {
-#  run "$BATS_TEST_DIRNAME"/docxbox lsl
-#  [ "$status" -ne 0 ]
-#  [ "Missing argument: DOCX filename" = "${lines[0]}" ]
-#}
+@test "Output of \"docxbox lsl filename.docx {missing argument}\" is an error message" {
+  run "$BATS_TEST_DIRNAME"/docxbox lsl
+  [ "$status" -ne 0 ]
+  [ "Missing argument: DOCX filename" = "${lines[0]}" ]
+}
 
-@test "\"docxbox lsl filename.docx searchString\" list all files containing given searchString" {
+@test "\"docxbox lsl filename.docx searchString\" lists all files containing given searchString" {
   "$BATS_TEST_DIRNAME"/docxbox lsl test/functional/tmp/cp_table_unordered_list_images.docx fonts | grep -c "word/document.xml"
   "$BATS_TEST_DIRNAME"/docxbox lsl test/functional/tmp/cp_table_unordered_list_images.docx fonts | grep -c "word/fontTable.xml"
   "$BATS_TEST_DIRNAME"/docxbox lsl test/functional/tmp/cp_table_unordered_list_images.docx fonts | grep -c "word/numbering.xml"
   "$BATS_TEST_DIRNAME"/docxbox lsl test/functional/tmp/cp_table_unordered_list_images.docx fonts | grep -c "word/styles.xml"
+}
+
+@test "\"docxbox ls filename.docx -l searchString\" lists all files containing given searchString" {
+  "$BATS_TEST_DIRNAME"/docxbox ls test/functional/tmp/cp_table_unordered_list_images.docx -l fonts | grep -c "word/document.xml"
+  "$BATS_TEST_DIRNAME"/docxbox ls test/functional/tmp/cp_table_unordered_list_images.docx -l fonts | grep -c "word/fontTable.xml"
+  "$BATS_TEST_DIRNAME"/docxbox ls test/functional/tmp/cp_table_unordered_list_images.docx -l fonts | grep -c "word/numbering.xml"
+  "$BATS_TEST_DIRNAME"/docxbox ls test/functional/tmp/cp_table_unordered_list_images.docx -l fonts | grep -c "word/styles.xml"
+}
+
+@test "\"docxbox ls filename.docx --locate searchString\" lists all files containing given searchString" {
+  "$BATS_TEST_DIRNAME"/docxbox ls test/functional/tmp/cp_table_unordered_list_images.docx --locate fonts | grep -c "word/document.xml"
+  "$BATS_TEST_DIRNAME"/docxbox ls test/functional/tmp/cp_table_unordered_list_images.docx --locate fonts | grep -c "word/fontTable.xml"
+  "$BATS_TEST_DIRNAME"/docxbox ls test/functional/tmp/cp_table_unordered_list_images.docx --locate fonts | grep -c "word/numbering.xml"
+  "$BATS_TEST_DIRNAME"/docxbox ls test/functional/tmp/cp_table_unordered_list_images.docx --locate fonts | grep -c "word/styles.xml"
+}
+
+@test "\"docxbox lsl filename.docx regular-expression\" lists all files containing given regular-expression" {
+  "$BATS_TEST_DIRNAME"/docxbox lsl test/functional/tmp/cp_bio_assay.docx "/[0-9A-Z]\{8\}/" | grep -c "docProps/core.xml"
+}
+
+@test "\"docxbox ls filename.docx -l regular-expression\" lists all files containing given regular-expression" {
+  "$BATS_TEST_DIRNAME"/docxbox ls test/functional/tmp/cp_bio_assay.docx -l "/[0-9A-Z]\{8\}/" | grep -c "docProps/core.xml"
+}
+
+@test "\"docxbox ls filename.docx --locate regular-expression\" lists all files containing given regular-expression" {
+  "$BATS_TEST_DIRNAME"/docxbox ls test/functional/tmp/cp_bio_assay.docx --locate "/[0-9A-Z]\{8\}/" | grep -c "docProps/core.xml"
 }
