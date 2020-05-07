7410a7d3b3f2d8430810b87e8cce60f836a33878
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-07 14:58:47 +0200

    refactored lorem.bats.sh

--- test/functional/lorem.bats.sh
+++ test/functional/lorem.bats.sh
@@ -7,17 +7,24 @@ load _helper
   [ "$status" -eq 0 ]
 }
 
-@test "Output of \"docxbox lsd {missing argument}\" is an error message" {
+@test "Output of \"docxbox lorem {missing argument}\" is an error message" {
   run "$BATS_TEST_DIRNAME"/docxbox lorem
   [ "$status" -ne 0 ]
   [ "Missing argument: Filename of DOCX to be extracted" = "${lines[0]}" ]
 }
 
 @test "With \"docxbox lorem filename.docx\" text gets replaced by dummy text" {
+  run "$BATS_TEST_DIRNAME"/docxbox lorem test/functional/tmp/cp_table_unordered_list_images.docx
+  [ "$status" -eq 0 ]
+
   "$BATS_TEST_DIRNAME"/docxbox txt test/functional/tmp/cp_table_unordered_list_images.docx | grep -vc "Culpa ad eiusmod"
 }
 
 @test "With \"docxbox lorem filename.docx newFilename.docx\" text gets replaced by dummy text and is saved to new file" {
-  "$BATS_TEST_DIRNAME"/docxbox lorem test/functional/tmp/cp_table_unordered_list_images.docx test/functional/tmp/lorem.docx
+  run "$BATS_TEST_DIRNAME"/docxbox lorem test/functional/tmp/cp_table_unordered_list_images.docx test/functional/tmp/lorem.docx
+  [ "$status" -eq 0 ]
+
+  "$BATS_TEST_DIRNAME"/docxbox txt test/functional/tmp/lorem.docx | grep -vc "Culpa ad eiusmod"
+
   ls test/functional/tmp | grep -c lorem.docx
 }
