efee524be5f100ae3eed21eb36f16fd419948bf1
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-14 08:33:31 +0200

    started refactoring testSuites to meet shell style guides standards +1

--- test/functional/lsdj.bats.sh
+++ test/functional/lsdj.bats.sh
@@ -7,6 +7,7 @@ load _helper
 
 base_command="docxbox lsdj filename.docx"
 path_docx="test/functional/tmp/cp_table_unordered_list_images.docx"
+long_description_json="the fields in the docx are listed as JSON"
 
 @test "Exit code of \"${base_command}\" is zero" {
   run "$BATS_TEST_DIRNAME"/docxbox lsdj $path_docx
@@ -28,7 +29,7 @@ path_docx="test/functional/tmp/cp_table_unordered_list_images.docx"
 }
 
 title="With \"docxbox ls filename.docx --fields --json\" "
-title+="${long_description_Json}"
+title+="${long_description_json}"
 @test "${title}" {
   pattern="table_unordered_list_images.docx-"
   "$BATS_TEST_DIRNAME/"docxbox ls $path_docx --fields --json | grep -c $pattern
@@ -37,7 +38,7 @@ title+="${long_description_Json}"
   "$BATS_TEST_DIRNAME/"docxbox ls $path_docx --fields --json | grep -c $pattern
 }
 
-@test "With \"docxbox ls filename.docx -dj\" ${long_description_Json}" {
+@test "With \"docxbox ls filename.docx -dj\" ${long_description_json}" {
   pattern="table_unordered_list_images.docx-"
   "$BATS_TEST_DIRNAME/"docxbox ls $path_docx -dj | grep -c $pattern
 
@@ -45,7 +46,7 @@ title+="${long_description_Json}"
   "$BATS_TEST_DIRNAME/"docxbox ls $path_docx -dj | grep -c $pattern
 }
 
-@test "With \"docxbox lsd filename.docx --json\" ${long_description_Json}" {
+@test "With \"docxbox lsd filename.docx --json\" ${long_description_json}" {
   pattern="table_unordered_list_images.docx-"
   "$BATS_TEST_DIRNAME/"docxbox lsd $path_docx --json | grep -c $pattern
 
