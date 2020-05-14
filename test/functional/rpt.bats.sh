a4bf4c4de3bd98f53dab219542abe2007032e7b9
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-14 10:29:20 +0200

    started refactoring testSuites to meet shell style guides standards +1

--- test/functional/rpt.bats.sh
+++ test/functional/rpt.bats.sh
@@ -5,42 +5,44 @@
 
 load _helper
 
+path_docx="test/functional/tmp/cp_table_unordered_list_images.docx"
+
+base_command="docxbox rpt filename.docx"
+
 @test "Output of \"docxbox rpt {missing filename}\" is an error message" {
   run "$BATS_TEST_DIRNAME"/docxbox rpt
   [ "$status" -ne 0 ]
   [ "Missing argument: DOCX filename" = "${lines[0]}" ]
 }
 
-@test "Output of \"docxbox rpt filename.docx {missing arguments}\" is an error message" {
-  path_docx="test/functional/tmp/cp_table_unordered_list_images.docx"
-
+@test "Output of \"${base_command} {missing arguments}\" is an error message" {
   run "$BATS_TEST_DIRNAME"/docxbox rpt $path_docx
   [ "$status" -ne 0 ]
   [ "Missing argument: String to be found (and replaced)" = "${lines[0]}" ]
 }
 
-@test "Output of \"docxbox rpt filename.docx stringToBeReplaced {missing argument}\" is an error message" {
-  path_docx="test/functional/tmp/cp_table_unordered_list_images.docx"
-
+missing_argument="stringToBeReplaced {missing argument}"
+@test "Output of \"${base_command} ${missing_argument}\" is an error message" {
   run "$BATS_TEST_DIRNAME"/docxbox rpt $path_docx toBeReplaced
   [ "$status" -ne 0 ]
   [ "Missing argument: Replacement" = "${lines[0]}" ]
 }
 
-# @TODO: reactivate once issue is resolved (tests prevent other tests from running)
-#@test "With \"docxbox rpt filename.docx stringToBeReplaced replacementString\" the stringToBeReplaced gets replaced" {
-#  path_docx="test/functional/tmp/cp_bio_assay.docx"
-#
-#  run "$BATS_TEST_DIRNAME"/docxbox rpt $path_docx Lorem Dorem
-#  [ "$status" -eq 0 ]
-#  "$BATS_TEST_DIRNAME"/docxbox txt $path_docx | grep -c Dorem
-#}
-#
-#@test "With \"docxbox rpt filename.docx stringToBeReplaced replacementString newFile.docx\" the stringToBeReplaced gets replaced and is saved to new file" {
-#  path_docx_in="test/functional/tmp/cp_bio_assay.docx"
-#  path_docx_out="test/functional/tmp/replacedString.docx"
-#
-#  run "$BATS_TEST_DIRNAME"/docxbox rpt $path_docx_in Lorem Dorem $path_docx_out
-#  [ "$status" -eq 0 ]
-#  "$BATS_TEST_DIRNAME"/docxbox txt test/functional/tmp/replacedString.docx | grep -c Dorem
-#}
+arguments="stringToBeReplaced replacementString"
+appendix="the stringToBeReplaced gets replaced"
+@test "With \"${base_command} ${arguments}\" ${appendix}" {
+  run "$BATS_TEST_DIRNAME"/docxbox rpt $path_docx Lorem Dorem
+  [ "$status" -eq 0 ]
+  "$BATS_TEST_DIRNAME"/docxbox txt $path_docx | grep -c Dorem
+}
+
+arguments_new_docx="stringToBeReplaced replacementString newFile.docx"
+appendix_new_docx="the stringToBeReplaced gets replaced and is saved to new file"
+@test "With \"${base_command} ${arguments_new_docx}\" ${appendix_new_docx}" {
+  path_docx_in="test/functional/tmp/cp_bio_assay.docx"
+  path_docx_out="test/functional/tmp/replacedString.docx"
+
+  run "$BATS_TEST_DIRNAME"/docxbox rpt $path_docx_in Lorem Dorem $path_docx_out
+  [ "$status" -eq 0 ]
+  "$BATS_TEST_DIRNAME"/docxbox txt $path_docx_out | grep -c Dorem
+}
