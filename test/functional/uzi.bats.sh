d6b6bad94f3d6b4830a594d79d163fbe515fe9d8
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-15 16:40:02 +0200

    started refactoring testSuites to meet shell style guides standards +1

--- test/functional/uzi.bats.sh
+++ test/functional/uzi.bats.sh
@@ -5,51 +5,52 @@
 
 load _helper
 
+docxbox=""$BATS_TEST_DIRNAME"/docxbox"
 path_docx="test/functional/tmp/cp_bio_assay.docx"
 
 unzipped_folder="cp_bio_assay.docx-extracted"
 
 @test "Output of \"docxbox uzi {missing argument}\" is an error message" {
-  run "$BATS_TEST_DIRNAME"/docxbox uzi
+  run "${docxbox}" uzi
   [ "$status" -ne 0 ]
   [ "Missing argument: Filename of DOCX to be extracted" = "${lines[0]}" ]
 }
 
 @test "With of \"docxbox uzi filename.docx\" all files are unzipped" {
-  run "$BATS_TEST_DIRNAME"/docxbox uzi "${path_docx}"
+  run "${docxbox}" uzi "${path_docx}"
   [ "$status" -eq 0 ]
 }
 
 @test "Unziped files are located in project root" {
-  ls | grep -c "${unzipped_folder}"
+  ls | grep --count "${unzipped_folder}"
 
   if [ -d "${unzipped_folder}" ]; then
-    rm -r "${unzipped_folder}";
+    rm --recursive "${unzipped_folder}";
   fi
 }
 
 @test "With of \"docxbox uz filename.docx -i\" all files are unzipped" {
-  run "$BATS_TEST_DIRNAME"/docxbox uz "${path_docx}" -i
+  run "${docxbox}" uz "${path_docx}" -i
   [ "$status" -eq 0 ]
 }
 
 @test "Unziped files are located in project root after running uz -i" {
-  ls | grep -c "${unzipped_folder}"
+  ls | grep --count "${unzipped_folder}"
 
   if [ -d "${unzipped_folder}" ]; then
-    rm -r "${unzipped_folder}";
+    rm --recursive "${unzipped_folder}";
   fi
 }
 
 @test "With of \"docxbox uz filename.docx --indent\" all files are unzipped" {
-  run "$BATS_TEST_DIRNAME"/docxbox uz "${path_docx}" --indent
+  run "${docxbox}" uz "${path_docx}" --indent
   [ "$status" -eq 0 ]
 }
 
 @test "Unziped files are located in project root after running uz --indent" {
-  ls | grep -c "${unzipped_folder}"
+  ls | grep --count "${unzipped_folder}"
 
   if [ -d "${unzipped_folder}" ]; then
-    rm -r "${unzipped_folder}";
+    rm --recursive "${unzipped_folder}";
   fi
 }
