048f4c3542d2ae918710d8af53dbbaef7c245f6c
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-15 14:45:54 +0200

    started refactoring testSuites to meet shell style guides standards +1

--- test/functional/lsm.bats.sh
+++ test/functional/lsm.bats.sh
@@ -10,7 +10,7 @@ path_docx="test/functional/tmp/cp_table_unordered_list_images.docx"
 base_command="docxbox lsm filename.docx"
 
 @test "Exit code of \"${base_command}\" is zero" {
-  run "$BATS_TEST_DIRNAME"/docxbox lsm $path_docx
+  run "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}"
   [ "$status" -eq 0 ]
 }
 
@@ -23,7 +23,7 @@ base_command="docxbox lsm filename.docx"
 @test "Output of \"${base_command}\" contains information about the xml schema" {
   xml_schema="xmlSchema: http://schemas.openxmlformats.org/officeDocument/2006"
 
-  run "$BATS_TEST_DIRNAME"/docxbox lsm $path_docx
+  run "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}"
   [ "$status" -eq 0 ]
   [ "$xml_schema" = "${lines[0]}" ]
 }
@@ -31,19 +31,19 @@ base_command="docxbox lsm filename.docx"
 title="Output of \"${base_command}\" "
 title+="contains information about the creation time and date"
 @test "${title}" {
-  run "$BATS_TEST_DIRNAME"/docxbox lsm $path_docx
+  run "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}"
   [ "$status" -eq 0 ]
   [ "created: 2017-08-02T11:09:18Z" = "${lines[1]}" ]
 }
 
 @test "Output of \"${base_command}\" contains language information" {
-  run "$BATS_TEST_DIRNAME"/docxbox lsm $path_docx
+  run "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}"
   [ "$status" -eq 0 ]
   [ "language: en-US" = "${lines[2]}" ]
 }
 
 @test "Output of \"${base_command}\" contains information about the revision" {
-  run "$BATS_TEST_DIRNAME"/docxbox lsm $path_docx
+  run "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}"
   [ "$status" -eq 0 ]
   [ "revision: 0" = "${lines[3]}" ]
 }
--- test/functional/lsmj.bats.sh
+++ test/functional/lsmj.bats.sh
@@ -14,7 +14,7 @@ description="contains information about the creation time and date"
 pattern="\"created\": \"2017-08-02T11:09:18Z\""
 
 @test "Exit code of \"${base_command}\" is zero" {
-  run "$BATS_TEST_DIRNAME"/docxbox lsmj $path_docx
+  run "$BATS_TEST_DIRNAME"/docxbox lsmj "${path_docx}"
   [ "$status" -eq 0 ]
 }
 
@@ -27,31 +27,31 @@ pattern="\"created\": \"2017-08-02T11:09:18Z\""
 @test "Output of \"${base_command}\" contains information about the xml schema" {
   pattern="\"xmlSchema\": \"http://schemas.openxmlformats.org/officeDocument/2006\""
 
-  "$BATS_TEST_DIRNAME"/docxbox lsmj $path_docx | grep -c "$pattern"
+  "$BATS_TEST_DIRNAME"/docxbox lsmj "${path_docx}" | grep -c "${pattern}"
 }
 
 @test "Output of \"docxbox lsm filename.docx --json\" ${description}" {
-  "$BATS_TEST_DIRNAME"/docxbox lsm $path_docx --json | grep -c "$pattern"
+  "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}" --json | grep -c "${pattern}"
 }
 
 @test "Output of \"docxbox lsm filename.docx -j\" ${description}" {
-  "$BATS_TEST_DIRNAME"/docxbox lsm $path_docx -j | grep -c "$pattern"
+  "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}" -j | grep -c "${pattern}"
 }
 
 @test "Output of \"docxbox ls filename.docx --meta --json\" ${description}" {
-  "$BATS_TEST_DIRNAME"/docxbox ls $path_docx --meta --json | grep -c "$pattern"
+  "$BATS_TEST_DIRNAME"/docxbox ls "${path_docx}" --meta --json | grep -c "${pattern}"
 }
 
 @test "Output of \"docxbox ls filename.docx -mj\" ${description}" {
-  "$BATS_TEST_DIRNAME"/docxbox ls $path_docx -mj | grep -c "$pattern"
+  "$BATS_TEST_DIRNAME"/docxbox ls "${path_docx}" -mj | grep -c "${pattern}"
 }
 
 @test "Output of \"${base_command}\" contains language information" {
   pattern="\"language\": \"en-US\""
 
-  "$BATS_TEST_DIRNAME"/docxbox lsmj $path_docx | grep -c "$pattern"
+  "$BATS_TEST_DIRNAME"/docxbox lsmj "${path_docx}" | grep -c "${pattern}"
 }
 
 @test "Output of \"${base_command}\" contains information about the revision" {
-  "$BATS_TEST_DIRNAME"/docxbox lsmj $path_docx | grep -c "\"revision\": \"0\""
+  "$BATS_TEST_DIRNAME"/docxbox lsmj "${path_docx}" | grep -c "\"revision\": \"0\""
 }
