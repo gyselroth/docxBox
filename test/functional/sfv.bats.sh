4d00fcd44c5b005fd8cc99a322bb434f12cdb79c
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-15 16:32:48 +0200

    started refactoring testSuites to meet shell style guides standards +1

--- test/functional/sfv.bats.sh
+++ test/functional/sfv.bats.sh
@@ -5,27 +5,28 @@
 
 load _helper
 
+docxbox=""$BATS_TEST_DIRNAME"/docxbox"
 path_docx="test/functional/tmp/cp_mergefields.docx"
 
 base_command="docxbox sfv"
 mergefield="MERGEFIELD  Schueler_Anrede"
 
 @test "Output of \"${base_command} {missing argument}\" is an error message" {
-  run "$BATS_TEST_DIRNAME"/docxbox sfv
+  run "${docxbox}" sfv
   [ "$status" -ne 0 ]
   [ "Missing argument: Filename of DOCX to be extracted" = "${lines[0]}" ]
 }
 
 missing_arguments="filename.docx {missing argument}"
 @test "Output of \"${base_command} ${missing_arguments}\" is an error message" {
-  run "$BATS_TEST_DIRNAME"/docxbox sfv "${path_docx}"
+  run "${docxbox}" sfv "${path_docx}"
   [ "$status" -ne 0 ]
   [ "Missing argument: Field identifier" = "${lines[0]}" ]
 }
 
 missing_value="filename.docx fieldIdentifier {missing argument}"
 @test "Output of \"${base_command} ${missing_value}\" is an error message" {
-  run "$BATS_TEST_DIRNAME"/docxbox sfv "${path_docx}" "${mergefield}"
+  run "${docxbox}" sfv "${path_docx}" "${mergefield}"
   [ "$status" -ne 0 ]
   [ "Missing argument: Value to be set" = "${lines[0]}" ]
 }
@@ -33,8 +34,8 @@ missing_value="filename.docx fieldIdentifier {missing argument}"
 arguments="filename.docx fieldIdentifier fieldValue"
 appendix="changes the value of the given field"
 @test "Output of \"${base_command} ${arguments}\" ${appendix}" {
-  run "$BATS_TEST_DIRNAME"/docxbox sfv "${path_docx}" "${mergefield}" foobar
+  run "${docxbox}" sfv "${path_docx}" "${mergefield}" foobar
   [ "$status" -eq 0 ]
 
-  "$BATS_TEST_DIRNAME"/docxbox txt "${path_docx}" | grep -c "foobar"
+  "${docxbox}" txt "${path_docx}" | grep --count "foobar"
 }
