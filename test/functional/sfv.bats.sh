e9d1b42417f0ea2db23fe66d933826390e2fcc98
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-15 14:57:20 +0200

    started refactoring testSuites to meet shell style guides standards +1

--- test/functional/sfv.bats.sh
+++ test/functional/sfv.bats.sh
@@ -18,14 +18,14 @@ mergefield="MERGEFIELD  Schueler_Anrede"
 
 missing_arguments="filename.docx {missing argument}"
 @test "Output of \"${base_command} ${missing_arguments}\" is an error message" {
-  run "$BATS_TEST_DIRNAME"/docxbox sfv $path_docx
+  run "$BATS_TEST_DIRNAME"/docxbox sfv "${path_docx}"
   [ "$status" -ne 0 ]
   [ "Missing argument: Field identifier" = "${lines[0]}" ]
 }
 
 missing_value="filename.docx fieldIdentifier {missing argument}"
 @test "Output of \"${base_command} ${missing_value}\" is an error message" {
-  run "$BATS_TEST_DIRNAME"/docxbox sfv $path_docx "$mergefield"
+  run "$BATS_TEST_DIRNAME"/docxbox sfv "${path_docx}" "${mergefield}"
   [ "$status" -ne 0 ]
   [ "Missing argument: Value to be set" = "${lines[0]}" ]
 }
@@ -33,8 +33,8 @@ missing_value="filename.docx fieldIdentifier {missing argument}"
 arguments="filename.docx fieldIdentifier fieldValue"
 appendix="changes the value of the given field"
 @test "Output of \"${base_command} ${arguments}\" ${appendix}" {
-  run "$BATS_TEST_DIRNAME"/docxbox sfv $path_docx "$mergefield" foobar
+  run "$BATS_TEST_DIRNAME"/docxbox sfv "${path_docx}" "${mergefield}" foobar
   [ "$status" -eq 0 ]
 
-  "$BATS_TEST_DIRNAME"/docxbox txt $path_docx | grep -c "foobar"
+  "$BATS_TEST_DIRNAME"/docxbox txt "${path_docx}" | grep -c "foobar"
 }
