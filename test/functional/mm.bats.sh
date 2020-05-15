8e5b72e130a9e715f0c182a1fa57ab5535b3f897
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-15 16:24:48 +0200

    started refactoring testSuites to meet shell style guides standards +1

--- test/functional/mm.bats.sh
+++ test/functional/mm.bats.sh
@@ -5,43 +5,44 @@
 
 load _helper
 
+docxbox=""$BATS_TEST_DIRNAME"/docxbox"
 path_docx="test/functional/tmp/cp_bio_assay.docx"
 
 base_command="docxbox mm filename.docx"
 
 @test "Output of \"docxbox mm {missing argument}\" is an error message" {
-  run "$BATS_TEST_DIRNAME"/docxbox mm
+  run "${docxbox}" mm
   [ "$status" -ne 0 ]
   [ "Missing argument: DOCX filename" = "${lines[0]}" ]
 }
 
 @test "Output of \"${base_command} {missing argument}\" is an error message" {
-  run "$BATS_TEST_DIRNAME"/docxbox mm "${path_docx}"
+  run "${docxbox}" mm "${path_docx}"
   [ "$status" -ne 0 ]
   [ "Missing argument: Meta attribute to be set" = "${lines[0]}" ]
 }
 
 title="the meta attribute title can be changed"
 @test "With \"${base_command} title {argument}\" ${title}" {
-  run "$BATS_TEST_DIRNAME"/docxbox mm "${path_docx}" title "someTitle"
+  run "${docxbox}" mm "${path_docx}" title "someTitle"
   [ "$status" -eq 0 ]
-  "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}" | grep -c "title: someTitle"
+  "${docxbox}" lsm "${path_docx}" | grep --count "title: someTitle"
 }
 
 creator="the meta attribute creator can be changed"
 @test "With \"${base_command} creator {argument}\" ${creator}" {
-  run "$BATS_TEST_DIRNAME"/docxbox mm "${path_docx}" creator "someOne"
+  run "${docxbox}" mm "${path_docx}" creator "someOne"
   [ "$status" -eq 0 ]
-  "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}" | grep -c "creator: someOne"
+  "${docxbox}" lsm "${path_docx}" | grep --count "creator: someOne"
 }
 
 last_modified_by="the meta attribute lastModifiedBy can be changed"
 @test "With \"${base_command} lastModifiedBy {argument}\" ${last_modified_by}" {
   pattern="lastModifiedBy: someOneElse"
 
-  run "$BATS_TEST_DIRNAME"/docxbox mm "${path_docx}" lastModifiedBy "someOneElse"
+  run "${docxbox}" mm "${path_docx}" lastModifiedBy "someOneElse"
   [ "$status" -eq 0 ]
-  "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}" | grep -c "${pattern}"
+  "${docxbox}" lsm "${path_docx}" | grep --count "${pattern}"
 }
 
 last_printed="the meta attribute lastPrinted can be changed"
@@ -49,39 +50,39 @@ last_printed="the meta attribute lastPrinted can be changed"
   print_date="2020-02-20T10:31:00Z"
   pattern="lastPrinted: 2020-02-20T10:31:00Z"
 
-  run "$BATS_TEST_DIRNAME"/docxbox mm "${path_docx}" lastPrinted $print_date
+  run "${docxbox}" mm "${path_docx}" lastPrinted $print_date
   [ "$status" -eq 0 ]
-  "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}" | grep -c "${pattern}"
+  "${docxbox}" lsm "${path_docx}" | grep --count "${pattern}"
 }
 
 language="the meta attribute language can be changed"
 @test "With \"${base_command} language {argument}\" ${language}" {
-  run "$BATS_TEST_DIRNAME"/docxbox mm "${path_docx}" language "de-CH"
+  run "${docxbox}" mm "${path_docx}" language "de-CH"
   [ "$status" -eq 0 ]
-  "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}" | grep -c "language: de-CH"
+  "${docxbox}" lsm "${path_docx}" | grep --count "language: de-CH"
 }
 
 modified="the meta attribute modified can be changed"
 @test "With \"${base_command} modified {argument}\" ${modified}" {
   pattern="modified: 2020-10-20T10:20:00Z"
 
-  run "$BATS_TEST_DIRNAME"/docxbox mm "${path_docx}" modified "2020-10-20T10:20:00Z"
+  run "${docxbox}" mm "${path_docx}" modified "2020-10-20T10:20:00Z"
   [ "$status" -eq 0 ]
-  "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}" | grep -c "${pattern}"
+  "${docxbox}" lsm "${path_docx}" | grep --count "${pattern}"
 }
 
 created="the meta attribute created can be changed"
 @test "With \"${base_command} created {argument}\" ${created}" {
   pattern="created: 2020-10-20T10:20:00Z"
 
-  run "$BATS_TEST_DIRNAME"/docxbox mm "${path_docx}" created "2020-10-20T10:20:00Z"
+  run "${docxbox}" mm "${path_docx}" created "2020-10-20T10:20:00Z"
   [ "$status" -eq 0 ]
-  "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}" | grep -c "${pattern}"
+  "${docxbox}" lsm "${path_docx}" | grep --count "${pattern}"
 }
 
 revision="the meta attribute revision can be changed"
 @test "With \"${base_command} revision {argument}\" ${revision}" {
-  run "$BATS_TEST_DIRNAME"/docxbox mm "${path_docx}" revision "25"
+  run "${docxbox}" mm "${path_docx}" revision "25"
   [ "$status" -eq 0 ]
-  "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}" | grep "revision: 25"
+  "${docxbox}" lsm "${path_docx}" | grep "revision: 25"
 }
