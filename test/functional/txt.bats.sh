8b6ab630e7ed488dc8afce41fbc000426e8e69e4
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-15 16:34:27 +0200

    started refactoring testSuites to meet shell style guides standards +1

--- test/functional/txt.bats.sh
+++ test/functional/txt.bats.sh
@@ -5,28 +5,29 @@
 
 load _helper
 
+docxbox=""$BATS_TEST_DIRNAME"/docxbox"
 path_docx="test/functional/tmp/cp_table_unordered_list_images.docx"
 
 base_command="docxbox txt filename.docx"
 
 @test "Output of \"docxbox txt {missing argument}\" is an error message" {
-  run "$BATS_TEST_DIRNAME"/docxbox txt
+  run "${docxbox}" txt
   [ "$status" -ne 0 ]
   [ "Missing argument: Filename of DOCX to be extracted" = "${lines[0]}" ]
 }
 
 @test "Output of \"${base_command}\" is the the plain text from given file" {
-  "$BATS_TEST_DIRNAME"/docxbox txt "${path_docx}" | grep -c "Officia"
+  "${docxbox}" txt "${path_docx}" | grep --count "Officia"
 }
 
 appendix="is the segmented plain text from given file"
 @test "Output of \"${base_command} -s \" ${appendix}" {
-  "$BATS_TEST_DIRNAME"/docxbox txt "${path_docx}" -s | wc -l | grep -c "105"
-  "$BATS_TEST_DIRNAME"/docxbox txt "${path_docx}" | wc -l | grep -c "65"
+  "${docxbox}" txt "${path_docx}" -s | wc --lines | grep --count "105"
+  "${docxbox}" txt "${path_docx}" | wc --lines | grep --count "65"
 }
 
 appendix="is the segmented plain text from given file"
 @test "Output of \"${base_command} --segments \" ${appendix}" {
-  "$BATS_TEST_DIRNAME"/docxbox txt "${path_docx}" --segments | wc -l | grep -c "105"
-  "$BATS_TEST_DIRNAME"/docxbox txt "${path_docx}" | wc -l | grep -c "65"
+  "${docxbox}" txt "${path_docx}" --segments | wc --lines | grep --count "105"
+  "${docxbox}" txt "${path_docx}" | wc --lines | grep --count "65"
 }
