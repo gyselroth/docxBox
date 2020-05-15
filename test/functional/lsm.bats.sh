edc6c57cf9b22cd19d5135701a4a0b57c5439ba5
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-15 16:23:47 +0200

    started refactoring testSuites to meet shell style guides standards +1

--- test/functional/lsm.bats.sh
+++ test/functional/lsm.bats.sh
@@ -5,17 +5,18 @@
 
 load _helper
 
+docxbox=""$BATS_TEST_DIRNAME"/docxbox"
 path_docx="test/functional/tmp/cp_table_unordered_list_images.docx"
 
 base_command="docxbox lsm filename.docx"
 
 @test "Exit code of \"${base_command}\" is zero" {
-  run "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}"
+  run "${docxbox}" lsm "${path_docx}"
   [ "$status" -eq 0 ]
 }
 
 @test "Output of \"docxbox lsm {missing argument}\" is an error message" {
-  run "$BATS_TEST_DIRNAME"/docxbox lsm
+  run "${docxbox}" lsm
   [ "$status" -ne 0 ]
   [ "Missing argument: Filename of DOCX to be extracted" = "${lines[0]}" ]
 }
@@ -23,7 +24,7 @@ base_command="docxbox lsm filename.docx"
 @test "Output of \"${base_command}\" contains information about the xml schema" {
   xml_schema="xmlSchema: http://schemas.openxmlformats.org/officeDocument/2006"
 
-  run "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}"
+  run "${docxbox}" lsm "${path_docx}"
   [ "$status" -eq 0 ]
   [ "$xml_schema" = "${lines[0]}" ]
 }
@@ -31,19 +32,19 @@ base_command="docxbox lsm filename.docx"
 title="Output of \"${base_command}\" "
 title+="contains information about the creation time and date"
 @test "${title}" {
-  run "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}"
+  run "${docxbox}" lsm "${path_docx}"
   [ "$status" -eq 0 ]
   [ "created: 2017-08-02T11:09:18Z" = "${lines[1]}" ]
 }
 
 @test "Output of \"${base_command}\" contains language information" {
-  run "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}"
+  run "${docxbox}" lsm "${path_docx}"
   [ "$status" -eq 0 ]
   [ "language: en-US" = "${lines[2]}" ]
 }
 
 @test "Output of \"${base_command}\" contains information about the revision" {
-  run "$BATS_TEST_DIRNAME"/docxbox lsm "${path_docx}"
+  run "${docxbox}" lsm "${path_docx}"
   [ "$status" -eq 0 ]
   [ "revision: 0" = "${lines[3]}" ]
 }
