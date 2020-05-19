b26117d73e8c131f05397bbbd6a5d1263f60b513
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-19 15:49:51 +0200

    added new testCase to diff.bats.sh to test the side-by-side comparison

--- test/functional/diff.bats.sh
+++ test/functional/diff.bats.sh
@@ -6,11 +6,16 @@
 
 load _helper
 
+docxbox=""$BATS_TEST_DIRNAME"/docxbox"
+
+path_docx_1="test/functional/tmp/cp_table_unordered_list_images.docx"
+path_docx_2="test/functional/tmp/cp_bio_assay.docx"
+
 base_command="docxbox diff filename.docx"
 error_message="is an error message"
 
 @test "Output of \"docxbox diff {missing argument}\" ${error_message}" {
-  run "$BATS_TEST_DIRNAME"/docxbox diff
+  run "${docxbox}" diff
   [ "$status" -ne 0 ]
   [ "Missing argument: DOCX file to compare with" = "${lines[0]}" ]
 }
@@ -18,19 +23,25 @@ error_message="is an error message"
 @test "Output of \"${base_command} {missing argument}\" ${error_message}" {
   path_docx="test/functional/tmp/cp_table_unordered_list_images.docx"
 
-  run "$BATS_TEST_DIRNAME"/docxbox diff "${path_docx}"
+  run "${docxbox}" diff "${path_docx}"
   [ "$status" -ne 0 ]
   [ "Missing argument: DOCX file to compare with" = "${lines[0]}" ]
 }
 
 title="Output of \"${base_command} otherFilename.docx {missing argument}\""
 @test "${title} ${error_message}" {
-  path_docx_1="test/functional/tmp/cp_table_unordered_list_images.docx"
-  path_docx_2="test/files/docx/table_unordered_list_images.docx"
-
   pattern="Missing argument: File within DOCX archives to be compared"
 
-  run "$BATS_TEST_DIRNAME"/docxbox diff "${path_docx_1}" "${path_docx_2}"
+  run "${docxbox}" diff "${path_docx_1}" "${path_docx_2}"
   [ "$status" -ne 0 ]
   [ "${pattern}" = "${lines[0]}" ]
+}
+
+description="a side by side view is displayed"
+@test "With \"${base_command} changedFilename.docx\" ${description}" {
+  path_changed_docx="test/functional/tmp/cp_table_unordered_list_images_v2.docx"
+  
+  run "${docxbox}" lorem "${path_docx_1}" "${path_changed_docx}"
+  
+  "${docxbox}" diff "${path_docx_1}" "${path_changed_docx}" word/document.xml | grep "|"
 }
