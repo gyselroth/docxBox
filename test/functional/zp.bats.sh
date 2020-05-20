41a2d86f86895e3d54bf7be53b2faca886da28ea
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-20 13:45:52 +0200

    refactored testSuite after changes in error handling +1

--- test/functional/zp.bats.sh
+++ test/functional/zp.bats.sh
@@ -7,26 +7,28 @@ load _helper
 
 docxbox=""$BATS_TEST_DIRNAME"/docxbox"
 
-unzipped_docx="test/functional/tmp/unzipped"
+unzipped_docx_directory="test/functional/tmp/unzipped"
 
 @test "Output of \"docxbox zp {missing argument}\" is an error message" {
+  pattern="docxBox Error - Missing argument: Path of directory to be zipped"
+
   run "${docxbox}" zp
   [ "$status" -ne 0 ]
-  [ "Missing argument: Path of directory to be zipped" = "${lines[0]}" ]
+  [ "${pattern}" = "${lines[0]}" ]
 }
 
 title="With \"docxbox zp directory /path-to-file/filename.docx\" "
 title+="a directory can be zipped into a docx"
 @test "$title" {
-  if [ ! -d "${unzipped_docx}" ]; then
-    mkdir "${unzipped_docx}";
+  if [ ! -d "${unzipped_docx_directory}" ]; then
+    mkdir "${unzipped_docx_directory}";
     path_docx="test/functional/tmp/cp_table_unordered_list_images.docx"
-    unzip $path_docx -d "${unzipped_docx}";
+    unzip $path_docx -d "${unzipped_docx_directory}";
   fi
 
   path_new_docx="test/functional/tmp/zp_table_unordered_list_images.docx"
 
-  run "${docxbox}" zp "${unzipped_docx}" "${path_new_docx}"
+  run "${docxbox}" zp "${unzipped_docx_directory}" "${path_new_docx}"
   [ "$status" -eq 0 ]
 
   ls test/functional/tmp | grep -c zp_table_unordered_list_images.docx
