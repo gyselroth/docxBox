809f4f336152cd69d70da6a661c26d136e2fd07d
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-20 12:27:36 +0200

    refactored testSuite after changes in error handling +1 implemented new testCase which asserts if an error message is displayed when providing a none-docx file

--- test/functional/rpt.bats.sh
+++ test/functional/rpt.bats.sh
@@ -13,20 +13,22 @@ base_command="docxbox rpt filename.docx"
 @test "Output of \"docxbox rpt {missing filename}\" is an error message" {
   run "${docxbox}" rpt
   [ "$status" -ne 0 ]
-  [ "Missing argument: DOCX filename" = "${lines[0]}" ]
+  [ "docxBox Error - Missing argument: DOCX filename" = "${lines[0]}" ]
 }
 
 @test "Output of \"${base_command} {missing arguments}\" is an error message" {
+  pattern="docxBox Error - Missing argument: String to be found (and replaced)"
+
   run "${docxbox}" rpt "${path_docx}"
   [ "$status" -ne 0 ]
-  [ "Missing argument: String to be found (and replaced)" = "${lines[0]}" ]
+  [ "${pattern}" = "${lines[0]}" ]
 }
 
 missing_argument="stringToBeReplaced {missing argument}"
 @test "Output of \"${base_command} ${missing_argument}\" is an error message" {
   run "${docxbox}" rpt "${path_docx}" toBeReplaced
   [ "$status" -ne 0 ]
-  [ "Missing argument: Replacement" = "${lines[0]}" ]
+  [ "docxBox Error - Missing argument: Replacement" = "${lines[0]}" ]
 }
 
 arguments="stringToBeReplaced replacementString"
@@ -45,4 +47,19 @@ appendix_new_docx="the stringToBeReplaced gets replaced and is saved to new file
   run "${docxbox}" rpt "${path_docx}" Lorem Dorem "${path_docx_out}"
   [ "$status" -eq 0 ]
   "${docxbox}" txt "${path_docx_out}" | grep --count Dorem
+}
+
+@test "Output of \"docxbox rpt wrong_file_type\" is an error message" {
+  pattern="docxBox Error - File is no ZIP archive:"
+  err_log="test/functional/tmp/err.log"
+  wrong_file_types=(
+  "test/functional/tmp/cp_lorem_ipsum.pdf"
+  "test/functional/tmp/cp_mock_csv.csv"
+  "test/functional/tmp/cp_mock_excel.xls")
+
+  for i in "${wrong_file_types[@]}"
+  do
+    "$BATS_TEST_DIRNAME"/docxbox rpt "${i}" Lorem Dorem 2>&1 | tee "${err_log}"
+    cat "${err_log}" | grep --count "${pattern}"
+  done
 }
