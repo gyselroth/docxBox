7c0584bee734a8ce6b1ba68c91697a04edff2784
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-20 10:44:30 +0200

    refactored testSuite after changes in error handling +1

--- test/functional/lsij.bats.sh
+++ test/functional/lsij.bats.sh
@@ -19,7 +19,7 @@ description="are contained images as JSON"
 @test "Output of \"docxbox lsij {missing argument}\" is an error message" {
   run "${docxbox}" lsij
   [ "$status" -ne 0 ]
-  [ "Missing argument: DOCX filename" = "${lines[0]}" ]
+  [ "docxBox Error - Missing argument: DOCX filename" = "${lines[0]}" ]
 }
 
 @test "Output of \"${base_command}\" is contained images as JSON" {
@@ -40,4 +40,18 @@ description="are contained images as JSON"
 
 @test "Output of \"docxbox ls filename.docx --images --json\" ${description}" {
   "${docxbox}" ls "${path_docx}" --images --json | grep --count "image1.jpeg"
+}
+
+@test "Output of \"docxbox lsij wrong_file_type\" is an error message" {
+  err_log="test/functional/tmp/err.log"
+  wrong_file_types=(
+  "test/functional/tmp/cp_lorem_ipsum.pdf"
+  "test/functional/tmp/cp_mock_csv.csv"
+  "test/functional/tmp/cp_mock_excel.xls")
+
+  for i in "${wrong_file_types[@]}"
+  do
+    "${docxbox}" lsij "${i}" 2>&1 | tee "${err_log}"
+    cat "${err_log}" | grep --count "docxBox Error - File is no ZIP archive:"
+  done
 }
