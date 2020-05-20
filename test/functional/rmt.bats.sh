9e4914031ae4a59a357a97ffeda905b5b799db9c
LucasBornhauser <bornhauser@gyselroth.com>
2020-05-20 12:52:36 +0200

    refactored testSuite after changes in error handling +1 implemented new testCase which asserts if an error message is displayed when providing a none-docx file

--- test/functional/rmt.bats.sh
+++ test/functional/rmt.bats.sh
@@ -10,18 +10,33 @@ docxbox=""$BATS_TEST_DIRNAME"/docxbox"
 @test "Output of \"docxbox rmt {missing filename}\" is an error message" {
   run "${docxbox}" rmt
   [ "$status" -ne 0 ]
-  [ "Missing argument: DOCX filename" = "${lines[0]}" ]
+  [ "docxBox Error - Missing argument: DOCX filename" = "${lines[0]}" ]
 }
 
 title="Output of \"docxbox rmt filename.docx {missing arguments}\" "
 title+="is an error message"
 @test "${title}" {
   path_docx="test/functional/tmp/cp_table_unordered_list_images.docx"
-  pattern="Missing argument: String left-hand-side of part to be removed"
+  pattern="docxBox Error - Missing argument: String left-hand-side of part to be removed"
 
   run "${docxbox}" rmt "${path_docx}"
   [ "$status" -ne 0 ]
   [ "${pattern}" = "${lines[0]}" ]
+}
+
+@test "Output of \"docxbox rmt wrong_file_type\" is an error message" {
+  pattern="docxBox Error - File is no ZIP archive:"
+  err_log="test/functional/tmp/err.log"
+  wrong_file_types=(
+  "test/functional/tmp/cp_lorem_ipsum.pdf"
+  "test/functional/tmp/cp_mock_csv.csv"
+  "test/functional/tmp/cp_mock_excel.xls")
+
+  for i in "${wrong_file_types[@]}"
+  do
+    "$BATS_TEST_DIRNAME"/docxbox rmt "${i}" Dolore incididunt 2>&1 | tee "${err_log}"
+    cat "${err_log}" | grep --count "${pattern}"
+  done
 }
 
 #@test "With \"docxbox rem filename.docx leftHandString rightHandString\" removes text between and including given strings" {
