bb36ce57f62b537a6602041e8288415376b72acb
LucasBornhauser <bornhauser@gyselroth.com>
2020-06-26 11:08:51 +0200

    refactored testSuite so valgrind can be used +1

--- test/functional/lsm.bats.sh
+++ test/functional/lsm.bats.sh
@@ -5,18 +5,27 @@
 
 load _helper
 
-DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+VALGRIND="valgrind -v --leak-check=full\
+ --log-file=test/assets/documents/other/mem-leak.log"
+
+if $IS_VALGRIND_TEST;
+then
+  DOCXBOX_BINARY="${VALGRIND} $BATS_TEST_DIRNAME/../tmp/docxbox"
+else
+  DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+fi
+
 path_docx="test/tmp/cp_table_unordered_list_images.docx"
 
 base_command="docxbox lsm filename.docx"
 
 @test "Exit code of \"${base_command}\" is zero" {
-  run "${DOCXBOX_BINARY}" lsm "${path_docx}"
+  run ${DOCXBOX_BINARY} lsm "${path_docx}"
   [ "$status" -eq 0 ]
 }
 
 @test "Output of \"docxbox lsm {missing argument}\" is an error message" {
-  run "${DOCXBOX_BINARY}" lsm
+  run ${DOCXBOX_BINARY} lsm
   [ "$status" -ne 0 ]
   [ "docxBox Error - Missing argument: Filename of DOCX to be extracted" = "${lines[0]}" ]
 }
@@ -24,7 +33,7 @@ base_command="docxbox lsm filename.docx"
 @test "Output of \"${base_command}\" contains information about the xml schema" {
   xml_schema="xmlSchema: http://schemas.openxmlformats.org/officeDocument/2006"
 
-  "${DOCXBOX_BINARY}" lsm "${path_docx}" | grep --count "${xml_schema}"
+  ${DOCXBOX_BINARY} lsm "${path_docx}" | grep --count "${xml_schema}"
 }
 
 title="Output of \"${base_command}\" "
@@ -32,24 +41,24 @@ title+="contains information about the creation time and date"
 @test "${title}" {
   created="created: 2020-06-18T10:30:11Z"
 
-  "${DOCXBOX_BINARY}" lsm "${path_docx}" | grep --count "${created}"
+  ${DOCXBOX_BINARY} lsm "${path_docx}" | grep --count "${created}"
 }
 
 @test "Output of \"${base_command}\" contains language information" {
-  "${DOCXBOX_BINARY}" lsm "${path_docx}" | grep --count "language: en-US"
+  ${DOCXBOX_BINARY} lsm "${path_docx}" | grep --count "language: en-US"
 }
 
 @test "Output of \"${base_command}\" contains information about the revision" {
-  "${DOCXBOX_BINARY}" lsm "${path_docx}" | grep --count "revision: 2"
+  ${DOCXBOX_BINARY} lsm "${path_docx}" | grep --count "revision: 2"
 }
 
 @test "Output of \"docxbox lsm nonexistent.docx\" is an error message" {
   err_log="test/tmp/err.log"
 
-  run "${DOCXBOX_BINARY}" lsm nonexistent.docx
+  run ${DOCXBOX_BINARY} lsm nonexistent.docx
   [ "$status" -ne 0 ]
 
-  "${DOCXBOX_BINARY}" lsm nonexistent.docx 2>&1 | tee "${err_log}"
+  ${DOCXBOX_BINARY} lsm nonexistent.docx 2>&1 | tee "${err_log}"
   cat "${err_log}" | grep --count "docxBox Error - File not found:"
 }
 
@@ -63,7 +72,7 @@ title+="contains information about the creation time and date"
 
   for i in "${wrong_file_types[@]}"
   do
-    "$BATS_TEST_DIRNAME"/docxbox lsm "${i}" 2>&1 | tee "${err_log}"
+    ${DOCXBOX_BINARY} lsm "${i}" 2>&1 | tee "${err_log}"
     cat "${err_log}" | grep --count "${pattern}"
   done
 }
--- test/functional/lsmj.bats.sh
+++ test/functional/lsmj.bats.sh
@@ -5,7 +5,16 @@
 
 load _helper
 
-DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+VALGRIND="valgrind -v --leak-check=full\
+ --log-file=test/assets/documents/other/mem-leak.log"
+
+if $IS_VALGRIND_TEST;
+then
+  DOCXBOX_BINARY="${VALGRIND} $BATS_TEST_DIRNAME/../tmp/docxbox"
+else
+  DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+fi
+
 path_docx="test/tmp/cp_table_unordered_list_images.docx"
 
 base_command="docxbox lsmj filename.docx"
@@ -15,14 +24,14 @@ description="contains information about the creation time and date"
 pattern="\"created\": \"2020-06-18T10:30:11Z\""
 
 @test "Exit code of \"${base_command}\" is zero" {
-  run "${DOCXBOX_BINARY}" lsmj "${path_docx}"
+  run ${DOCXBOX_BINARY} lsmj "${path_docx}"
   [ "$status" -eq 0 ]
 }
 
 @test "Output of \"docxbox lsmj {missing argument}\" is an error message" {
   pattern="docxBox Error - Missing argument: Filename of DOCX to be extracted"
 
-  run "${DOCXBOX_BINARY}" lsmj
+  run ${DOCXBOX_BINARY} lsmj
   [ "$status" -ne 0 ]
   [ "${pattern}" = "${lines[0]}" ]
 }
@@ -30,42 +39,42 @@ pattern="\"created\": \"2020-06-18T10:30:11Z\""
 @test "Output of \"${base_command}\" contains information about the xml schema" {
   pattern="\"xmlSchema\": \"http://schemas.openxmlformats.org/officeDocument/2006\""
 
-  "${DOCXBOX_BINARY}" lsmj "${path_docx}" | grep --count "${pattern}"
+  ${DOCXBOX_BINARY} lsmj "${path_docx}" | grep --count "${pattern}"
 }
 
 @test "Output of \"docxbox lsm filename.docx --json\" ${description}" {
-  "${DOCXBOX_BINARY}" lsm "${path_docx}" --json | grep --count "${pattern}"
+  ${DOCXBOX_BINARY} lsm "${path_docx}" --json | grep --count "${pattern}"
 }
 
 @test "Output of \"docxbox lsm filename.docx -j\" ${description}" {
-  "${DOCXBOX_BINARY}" lsm "${path_docx}" -j | grep --count "${pattern}"
+  ${DOCXBOX_BINARY} lsm "${path_docx}" -j | grep --count "${pattern}"
 }
 
 @test "Output of \"docxbox ls filename.docx --meta --json\" ${description}" {
-  "${DOCXBOX_BINARY}" ls "${path_docx}" --meta --json | grep --count "${pattern}"
+  ${DOCXBOX_BINARY} ls "${path_docx}" --meta --json | grep --count "${pattern}"
 }
 
 @test "Output of \"docxbox ls filename.docx -mj\" ${description}" {
-  "${DOCXBOX_BINARY}" ls "${path_docx}" -mj | grep --count "${pattern}"
+  ${DOCXBOX_BINARY} ls "${path_docx}" -mj | grep --count "${pattern}"
 }
 
 @test "Output of \"${base_command}\" contains language information" {
   pattern="\"language\": \"en-US\""
 
-  "${DOCXBOX_BINARY}" lsmj "${path_docx}" | grep --count "${pattern}"
+  ${DOCXBOX_BINARY} lsmj "${path_docx}" | grep --count "${pattern}"
 }
 
 @test "Output of \"${base_command}\" contains information about the revision" {
-  "${DOCXBOX_BINARY}" lsmj "${path_docx}" | grep --count "\"revision\": \"2\""
+  ${DOCXBOX_BINARY} lsmj "${path_docx}" | grep --count "\"revision\": \"2\""
 }
 
 @test "Output of \"docxbox lsmj nonexistent.docx\" is an error message" {
   err_log="test/tmp/err.log"
 
-  run "${DOCXBOX_BINARY}" lsmj nonexistent.docx
+  run ${DOCXBOX_BINARY} lsmj nonexistent.docx
   [ "$status" -ne 0 ]
 
-  "${DOCXBOX_BINARY}" lsmj nonexistent.docx 2>&1 | tee "${err_log}"
+  ${DOCXBOX_BINARY} lsmj nonexistent.docx 2>&1 | tee "${err_log}"
   cat "${err_log}" | grep --count "docxBox Error - File not found:"
 }
 
@@ -79,7 +88,7 @@ pattern="\"created\": \"2020-06-18T10:30:11Z\""
 
   for i in "${wrong_file_types[@]}"
   do
-    "${DOCXBOX_BINARY}" lsmj "${i}" 2>&1 | tee "${err_log}"
+    ${DOCXBOX_BINARY} lsmj "${i}" 2>&1 | tee "${err_log}"
     cat "${err_log}" | grep --count "${pattern}"
   done
 }
