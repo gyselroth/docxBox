6fcb2c5855415d4ea39415cf6a1acedf60818224
LucasBornhauser <bornhauser@gyselroth.com>
2020-06-26 11:39:01 +0200

    refactored testSuite so valgrind can be used +1

--- test/functional/cat.bats.sh
+++ test/functional/cat.bats.sh
@@ -5,23 +5,31 @@
 
 load _helper
 
-DOCXBOX_BINARY="$BATS_TEST_DIRNAME/docxbox"
+VALGRIND="valgrind -v --leak-check=full\
+ --log-file=test/assets/documents/other/mem-leak.log"
+
+if $IS_VALGRIND_TEST;
+then
+  DOCXBOX_BINARY="${VALGRIND} $BATS_TEST_DIRNAME/../tmp/docxbox"
+else
+  DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+fi
 
 path_docx="test/tmp/cp_table_unordered_list_images.docx"
 path_xml="word/document.xml"
 
 @test "Output of \"docxbox cat {missing argument}\" is an error message" {
-  run "${DOCXBOX_BINARY}" cat
+  run ${DOCXBOX_BINARY} cat
   [ "$status" -ne 0 ]
   [ "docxBox Error - Missing argument: DOCX filename" = "${lines[0]}" ]
 }
 
 @test "Output of \"docxbox cat filename.docx {missing argument}\" is an error message" {
-  run "${DOCXBOX_BINARY}" cat "${path_docx}"
+  run ${DOCXBOX_BINARY} cat "${path_docx}"
   [ "$status" -ne 0 ]
   [ "docxBox Error - Missing argument: File to be displayed" = "${lines[0]}" ]
 }
 
 @test "With \"docxbox cat filename.docx filename.xml\" the XML is displayed" {
-  "${DOCXBOX_BINARY}" cat "${path_docx}" "${path_xml}" | grep "^[[:space:]]\{4\}"
+  ${DOCXBOX_BINARY} cat "${path_docx}" "${path_xml}" | grep "^[[:space:]]\{4\}"
 }
