22de8518b5318831a0ba55c769e1cf4270c03c6d
LucasBornhauser <bornhauser@gyselroth.com>
2020-06-26 11:50:37 +0200

    refactored testSuite so valgrind can be used +1

--- test/functional/lorem.bats.sh
+++ test/functional/lorem.bats.sh
@@ -5,20 +5,29 @@
 
 load _helper
 
-DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+VALGRIND="valgrind -v --leak-check=full\
+ --log-file=test/assets/documents/other/mem-leak.log"
+
+if $IS_VALGRIND_TEST;
+then
+  DOCXBOX_BINARY="${VALGRIND} $BATS_TEST_DIRNAME/../tmp/docxbox"
+else
+  DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+fi
+
 base_command="docxbox lorem filename.docx"
 
 @test "Exit code of \"${base_command}\" is zero" {
   path_docx="test/tmp/cp_table_unordered_list_images.docx"
 
-  run "${DOCXBOX_BINARY}" lorem "${path_docx}"
+  run ${DOCXBOX_BINARY} lorem "${path_docx}"
   [ "$status" -eq 0 ]
 }
 
 @test "Output of \"docxbox lorem {missing argument}\" is an error message" {
   pattern="docxBox Error - Missing argument: Filename of DOCX to be extracted"
 
-  run "${DOCXBOX_BINARY}" lorem
+  run ${DOCXBOX_BINARY} lorem
   [ "$status" -ne 0 ]
   [ "${pattern}" = "${lines[0]}" ]
 }
@@ -27,10 +36,10 @@ base_command="docxbox lorem filename.docx"
   path_docx="test/tmp/cp_table_unordered_list_images.docx"
   pattern="Culpa ad eiusmod"
 
-  run "${DOCXBOX_BINARY}" lorem "${path_docx}"
+  run ${DOCXBOX_BINARY} lorem "${path_docx}"
   [ "$status" -eq 0 ]
 
-  "${DOCXBOX_BINARY}" txt "${path_docx}" | grep --invert-match --count "${pattern}"
+  ${DOCXBOX_BINARY} txt "${path_docx}" | grep --invert-match --count "${pattern}"
 }
 
 title="With \"${base_command} newFilename.docx\" "
@@ -39,17 +48,17 @@ title+="text gets replaced by dummy text and is saved to new file"
   path_docx_1="test/tmp/cp_table_unordered_list_images.docx"
   path_docx_2="test/tmp/lorem.docx"
 
-  "${DOCXBOX_BINARY}" lorem "${path_docx_1}" "${path_docx_2}"
+  ${DOCXBOX_BINARY} lorem "${path_docx_1}" "${path_docx_2}"
   ls test/tmp | grep -c lorem.docx
 }
 
 @test "Output of \"docxbox lorem nonexistent.docx\" is an error message" {
   err_log="test/tmp/err.log"
 
-  run "${DOCXBOX_BINARY}" lorem nonexistent.docx
+  run ${DOCXBOX_BINARY} lorem nonexistent.docx
   [ "$status" -ne 0 ]
 
-  "${DOCXBOX_BINARY}" lorem nonexistent.docx 2>&1 | tee "${err_log}"
+  ${DOCXBOX_BINARY} lorem nonexistent.docx 2>&1 | tee "${err_log}"
   cat "${err_log}" | grep --count "docxBox Error - File not found:"
 }
 
@@ -63,7 +72,7 @@ title+="text gets replaced by dummy text and is saved to new file"
 
   for i in "${wrong_file_types[@]}"
   do
-    "${DOCXBOX_BINARY}" lorem "${i}" 2>&1 | tee "${err_log}"
+    ${DOCXBOX_BINARY} lorem "${i}" 2>&1 | tee "${err_log}"
     cat "${err_log}" | grep --count "${pattern}"
   done
 }
