963bf00df95edd51fe202f301bb9ee68ea9c1f4d
LucasBornhauser <bornhauser@gyselroth.com>
2020-06-26 11:20:30 +0200

    refactored testSuite so valgrind can be used +1

--- test/functional/sfv.bats.sh
+++ test/functional/sfv.bats.sh
@@ -5,7 +5,16 @@
 
 load _helper
 
-DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+VALGRIND="valgrind -v --leak-check=full\
+ --log-file=test/assets/documents/other/mem-leak.log"
+
+if $IS_VALGRIND_TEST;
+then
+  DOCXBOX_BINARY="${VALGRIND} $BATS_TEST_DIRNAME/../tmp/docxbox"
+else
+  DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+fi
+
 path_docx="test/tmp/cp_mergefields.docx"
 
 base_command="docxbox sfv"
@@ -17,21 +26,21 @@ mergefield_footer="MERGEFIELD  Mergefield_Footer"
 @test "Output of \"${base_command} {missing argument}\" is an error message" {
   pattern="docxBox Error - Missing argument: Filename of DOCX to be extracted"
 
-  run "${DOCXBOX_BINARY}" sfv
+  run ${DOCXBOX_BINARY} sfv
   [ "$status" -ne 0 ]
   [ "${pattern}" = "${lines[0]}" ]
 }
 
 missing_arguments="filename.docx {missing argument}"
 @test "Output of \"${base_command} ${missing_arguments}\" is an error message" {
-  run "${DOCXBOX_BINARY}" sfv "${path_docx}"
+  run ${DOCXBOX_BINARY} sfv "${path_docx}"
   [ "$status" -ne 0 ]
   [ "docxBox Error - Missing argument: Field identifier" = "${lines[1]}" ]
 }
 
 missing_value="filename.docx fieldIdentifier {missing argument}"
 @test "Output of \"${base_command} ${missing_value}\" is an error message" {
-  run "${DOCXBOX_BINARY}" sfv "${path_docx}" "${mergefield}"
+  run ${DOCXBOX_BINARY} sfv "${path_docx}" "${mergefield}"
   [ "$status" -ne 0 ]
   [ "docxBox Error - Missing argument: Value to be set" = "${lines[1]}" ]
 }
@@ -39,33 +48,33 @@ missing_value="filename.docx fieldIdentifier {missing argument}"
 arguments="filename.docx fieldIdentifier fieldValue"
 appendix=" the value of the given field is changed"
 @test "With \"${base_command} ${arguments}\" ${appendix}" {
-  run "${DOCXBOX_BINARY}" sfv "${path_docx}" "${mergefield}" foobar
+  run ${DOCXBOX_BINARY} sfv "${path_docx}" "${mergefield}" foobar
   [ "$status" -eq 0 ]
 
-  "${DOCXBOX_BINARY}" txt "${path_docx}" | grep --count "foobar"
+  ${DOCXBOX_BINARY} txt "${path_docx}" | grep --count "foobar"
 }
 
 @test "With \"${base_command} ${arguments}\" the value of the mergefield in the header gets changed" {
-  run "${DOCXBOX_BINARY}" sfv "${path_docx}" "${mergefield_header}" foobar
+  run ${DOCXBOX_BINARY} sfv "${path_docx}" "${mergefield_header}" foobar
   [ "$status" -eq 0 ]
 
-  "${DOCXBOX_BINARY}" txt "${path_docx}" | grep --count "foobar"
+  ${DOCXBOX_BINARY} txt "${path_docx}" | grep --count "foobar"
 }
 
 @test "With \"${base_command} ${arguments}\" the value of the mergefield in the footer gets changed" {
-  run "${DOCXBOX_BINARY}" sfv "${path_docx}" "${mergefield_footer}" foobar
+  run ${DOCXBOX_BINARY} sfv "${path_docx}" "${mergefield_footer}" foobar
   [ "$status" -eq 0 ]
 
-  "${DOCXBOX_BINARY}" txt "${path_docx}" | grep --count "foobar"
+  ${DOCXBOX_BINARY} txt "${path_docx}" | grep --count "foobar"
 }
 
 @test "Output of \"docxbox sfv nonexistent.docx\" is an error message" {
   err_log="test/tmp/err.log"
 
-  run "${DOCXBOX_BINARY}" sfv nonexistent.docx
+  run ${DOCXBOX_BINARY} sfv nonexistent.docx
   [ "$status" -ne 0 ]
 
-  "${DOCXBOX_BINARY}" sfv nonexistent.docx "${mergefield}" foobar 2>&1 | tee "${err_log}"
+  ${DOCXBOX_BINARY} sfv nonexistent.docx "${mergefield}" foobar 2>&1 | tee "${err_log}"
   cat "${err_log}" | grep --count "docxBox Error - File not found:"
 }
 
@@ -81,7 +90,7 @@ title+="is an error message"
 
   for i in "${wrong_file_types[@]}"
   do
-    "${DOCXBOX_BINARY}" lorem "${i}" "${mergefield}" foobar 2>&1 | tee "${err_log}"
+    ${DOCXBOX_BINARY} lorem "${i}" "${mergefield}" foobar 2>&1 | tee "${err_log}"
     cat "${err_log}" | grep --count "${pattern}"
   done
 }
