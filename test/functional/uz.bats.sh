006793d3cd5e4eb0e6bf6deb7614e745796808e8
LucasBornhauser <bornhauser@gyselroth.com>
2020-06-26 11:23:42 +0200

    refactored testSuite so valgrind can be used +1

--- test/functional/uz.bats.sh
+++ test/functional/uz.bats.sh
@@ -5,14 +5,22 @@
 
 load _helper
 
-DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+VALGRIND="valgrind -v --leak-check=full\
+ --log-file=test/assets/documents/other/mem-leak.log"
+
+if $IS_VALGRIND_TEST;
+then
+  DOCXBOX_BINARY="${VALGRIND} $BATS_TEST_DIRNAME/../tmp/docxbox"
+else
+  DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+fi
 
 unzipped_folder="cp_bio_assay.docx-extracted"
 
 @test "Output of \"docxbox uz {missing argument}\" is an error message" {
   pattern="docxBox Error - Missing argument: Filename of DOCX to be extracted"
 
-  run "${DOCXBOX_BINARY}" uz
+  run ${DOCXBOX_BINARY} uz
   [ "$status" -ne 0 ]
   [ "${pattern}" = "${lines[0]}" ]
 }
@@ -20,10 +28,10 @@ unzipped_folder="cp_bio_assay.docx-extracted"
 @test "Output of \"docxbox uz nonexistent.docx\" is an error message" {
   err_log="test/tmp/err.log"
 
-  run "${DOCXBOX_BINARY}" uz nonexistent.docx
+  run ${DOCXBOX_BINARY} uz nonexistent.docx
   [ "$status" -ne 0 ]
 
-  "${DOCXBOX_BINARY}" uz nonexistent.docx 2>&1 | tee "${err_log}"
+  ${DOCXBOX_BINARY} uz nonexistent.docx 2>&1 | tee "${err_log}"
   cat "${err_log}" | grep --count "docxBox Error - File not found:"
 }
 
@@ -37,7 +45,7 @@ unzipped_folder="cp_bio_assay.docx-extracted"
 
   for i in "${wrong_file_types[@]}"
   do
-    "${DOCXBOX_BINARY}" uz "${i}" 2>&1 | tee "${err_log}"
+    ${DOCXBOX_BINARY} uz "${i}" 2>&1 | tee "${err_log}"
     cat "${err_log}" | grep --count "${pattern}"
   done
 }
@@ -45,7 +53,7 @@ unzipped_folder="cp_bio_assay.docx-extracted"
 @test "With \"docxbox uz filename.docx\" all files are unziped" {
   pattern="^[[:space:]]\{4\}"
 
-  run "${DOCXBOX_BINARY}" uz test/tmp/cp_bio_assay.docx
+  run ${DOCXBOX_BINARY} uz test/tmp/cp_bio_assay.docx
   [ "$status" -eq 0 ]
 
   cat "${unzipped_folder}/word/document.xml" | grep --invert-match "${pattern}"
