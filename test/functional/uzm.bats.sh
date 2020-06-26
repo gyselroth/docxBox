a30e38e15700f3a9c19a268d1e3cfc870e8a548b
LucasBornhauser <bornhauser@gyselroth.com>
2020-06-26 11:26:36 +0200

    refactored testSuite so valgrind can be used +1

--- test/functional/uzm.bats.sh
+++ test/functional/uzm.bats.sh
@@ -5,7 +5,16 @@
 
 load _helper
 
-DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+VALGRIND="valgrind -v --leak-check=full\
+ --log-file=test/assets/documents/other/mem-leak.log"
+
+if $IS_VALGRIND_TEST;
+then
+  DOCXBOX_BINARY="${VALGRIND} $BATS_TEST_DIRNAME/../tmp/docxbox"
+else
+  DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+fi
+
 path_docx="test/tmp/cp_table_unordered_list_images.docx"
 
 description="only media files are extracted"
@@ -14,7 +23,7 @@ unzipped_docx="cp_table_unordered_list_images.docx-media-extracted"
 @test "Output of \"docxbox uzm {missing argument}\" is an error message" {
   pattern="docxBox Error - Missing argument: Filename of DOCX to be extracted"
 
-  run "${DOCXBOX_BINARY}" uzm
+  run ${DOCXBOX_BINARY} uzm
   [ "$status" -ne 0 ]
   [ "${pattern}" = "${lines[0]}" ]
 }
@@ -22,10 +31,10 @@ unzipped_docx="cp_table_unordered_list_images.docx-media-extracted"
 @test "Output of \"docxbox uzm nonexistent.docx\" is an error message" {
   err_log="test/tmp/err.log"
 
-  run "${DOCXBOX_BINARY}" uzm nonexistent.docx
+  run ${DOCXBOX_BINARY} uzm nonexistent.docx
   [ "$status" -ne 0 ]
 
-  "${DOCXBOX_BINARY}" uzm nonexistent.docx 2>&1 | tee "${err_log}"
+  ${DOCXBOX_BINARY} uzm nonexistent.docx 2>&1 | tee "${err_log}"
   cat "${err_log}" | grep --count "docxBox Error - File not found:"
 }
 
@@ -39,13 +48,13 @@ unzipped_docx="cp_table_unordered_list_images.docx-media-extracted"
 
   for i in "${wrong_file_types[@]}"
   do
-    "${DOCXBOX_BINARY}" uzm "${i}" 2>&1 | tee "${err_log}"
+    ${DOCXBOX_BINARY} uzm "${i}" 2>&1 | tee "${err_log}"
     cat "${err_log}" | grep --count "${pattern}"
   done
 }
 
 @test "With \"docxbox uzm filename.docx\" ${description}" {
-  run "${DOCXBOX_BINARY}" uzm "${path_docx}"
+  run ${DOCXBOX_BINARY} uzm "${path_docx}"
 }
 
 @test "Unzipped files are located in project root" {
@@ -57,7 +66,7 @@ unzipped_docx="cp_table_unordered_list_images.docx-media-extracted"
 }
 
 @test "With \"docxbox uz filename.docx --media\" ${description}" {
-  run "${DOCXBOX_BINARY}" uz "${path_docx}" --media
+  run ${DOCXBOX_BINARY} uz "${path_docx}" --media
 }
 
 @test "Unzipped files are located in project root after running uz --media " {
@@ -69,7 +78,7 @@ unzipped_docx="cp_table_unordered_list_images.docx-media-extracted"
 }
 
 @test "With \"docxbox uz filename.docx -m\" ${description}" {
-  run "${DOCXBOX_BINARY}" uz "${path_docx}" -m
+  run ${DOCXBOX_BINARY} uz "${path_docx}" -m
 }
 
 @test "Unzipped files are located in project root after running uz -m" {
