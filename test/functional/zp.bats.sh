9b5ecc14eaccef1d7393fe577e70416a1deea8d1
LucasBornhauser <bornhauser@gyselroth.com>
2020-06-26 11:29:22 +0200

    refactored testSuite so valgrind can be used +1

--- test/functional/zp.bats.sh
+++ test/functional/zp.bats.sh
@@ -5,14 +5,22 @@
 
 load _helper
 
-DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+VALGRIND="valgrind -v --leak-check=full\
+ --log-file=test/assets/documents/other/mem-leak.log"
+
+if $IS_VALGRIND_TEST;
+then
+  DOCXBOX_BINARY="${VALGRIND} $BATS_TEST_DIRNAME/../tmp/docxbox"
+else
+  DOCXBOX_BINARY="$BATS_TEST_DIRNAME/../tmp/docxbox"
+fi
 
 unzipped_docx_directory="test/tmp/unzipped"
 
 @test "Output of \"docxbox zp {missing argument}\" is an error message" {
   pattern="docxBox Error - Missing argument: Path of directory to be zipped"
 
-  run "${DOCXBOX_BINARY}" zp
+  run ${DOCXBOX_BINARY} zp
   [ "$status" -ne 0 ]
   [ "${pattern}" = "${lines[0]}" ]
 }
@@ -20,7 +28,7 @@ unzipped_docx_directory="test/tmp/unzipped"
 @test "Output of \"docxbox zp directory {missing argument}\" is an error message" {
   pattern="docxBox Error - Missing argument: Filename of docx to be created"
 
-  run "${DOCXBOX_BINARY}" zp "${unzipped_docx_directory}"
+  run ${DOCXBOX_BINARY} zp "${unzipped_docx_directory}"
   [ "$status" -ne 0 ]
   [ "${pattern}" = "${lines[0]}" ]
 }
@@ -36,7 +44,7 @@ title+="a directory can be zipped into a docx"
 
   path_new_docx="test/tmp/zp_table_unordered_list_images.docx"
 
-  run "${DOCXBOX_BINARY}" zp "${unzipped_docx_directory}" "${path_new_docx}"
+  run ${DOCXBOX_BINARY} zp "${unzipped_docx_directory}" "${path_new_docx}"
   [ "$status" -eq 0 ]
 
   ls test/tmp | grep -c zp_table_unordered_list_images.docx
