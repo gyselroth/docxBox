dbc93657edd285462923bda10ac9fcf93ccefa16
LucasBornhauser <bornhauser@gyselroth.com>
2020-07-07 15:19:26 +0200

    extended batch.bats.sh - added new stable test cases for mm rmt and rpt commands

--- test/functional/batch.bats.sh
+++ test/functional/batch.bats.sh
@@ -19,9 +19,12 @@ else
 fi
 
 PATH_DOCX="test/tmp/cp_table_unordered_list_images.docx"
+PATH_DOCX_PLAINTEXT="test/tmp/cp_plain_text.docx"
 BASE_COMMAND="docxbox batch filename.docx"
 
-@test "Output of \"docxbox batch {missing argument}\" is an error message" {
+title_one_missing_argument="Case 1: Output of \"docxbox batch \
+{missing argument}\" is an error message"
+@test "${title_one_missing_argument}" {
   run ${DOCXBOX_BINARY} batch
   [ "$status" -ne 0 ]
   [ "docxBox Error - Missing argument: DOCX file" = "${lines[0]}" ]
@@ -29,7 +32,9 @@ BASE_COMMAND="docxbox batch filename.docx"
   check_for_valgrind_error
 }
 
-@test "Output of \"${BASE_COMMAND} {missing argument}\" is an error message" {
+title_two_missing_arguments="Case 2:Output of \"${BASE_COMMAND} \
+{missing argument}\" is an error message"
+@test "${title_two_missing_arguments}" {
   run ${DOCXBOX_BINARY} batch "${PATH_DOCX}"
   [ "$status" -ne 0 ]
   [ "docxBox Error - Missing argument: Batch commands JSON" = "${lines[0]}" ]
@@ -37,15 +42,14 @@ BASE_COMMAND="docxbox batch filename.docx"
   check_for_valgrind_error
 }
 
-# TODO(Lucas): Add testcase batch {1:mm}
-# TODO(Lucas): Add testcase batch {1:rpt}
-# TODO(Lucas): Add testcase batch {1:rmt}
 # TODO(Lucas): Add testcase batch {1:mm,2:rpt}
 # TODO(Lucas): Add testcase batch {1:mm,2:rmt}
 
 APPENDIX="a batch sequence can be executed"
-@test "With \"${BASE_COMMAND} batch_sequence_as_JSON\" ${APPENDIX} and the title gets changed" {
-  batch="{\"1\":{\"mm\":[\"title\",\"foo\"]},\"2\":{\"rpt\":[\"bar\",\"baz\"]},\"3\":{\"rpt\":[\"qux\",{\"h1\":{\"text\":\"Quux\"}}]}}"
+title_mm="Case 3: With \"${BASE_COMMAND} batch_sequence_as_JSON\" ${APPENDIX} \
+and the meta attribute title gets changed"
+@test "${title_mm}" {
+  batch="{\"1\":{\"mm\":[\"title\",\"foo\"]}}"
   run ${DOCXBOX_BINARY} batch "${PATH_DOCX}" "${batch}"
 
   check_for_valgrind_error
@@ -53,22 +57,36 @@ APPENDIX="a batch sequence can be executed"
   ${DOCXBOX_BINARY} lsm ${PATH_DOCX} | grep --count "title: foo"
 }
 
-@test "With \"${BASE_COMMAND} batch_sequence_as_JSON\" ${APPENDIX} and a string can be replaced" {
-  batch="{\"1\":{\"mm\":[\"title\",\"foo\"]},\"2\":{\"rpt\":[\"bar\",\"baz\"]},\"3\":{\"rpt\":[\"qux\",{\"h1\":{\"text\":\"Quux\"}}]}}"
-  run ${DOCXBOX_BINARY} batch "${PATH_DOCX}" "${batch}"
+title_rpt="Case 4: With \"${BASE_COMMAND} batch_sequence_as_JSON\" ${APPENDIX} \
+and a string can be replaced with the \"rpt\" command"
+@test "${title_rpt}" {
+  batch="{\"1\":{\"rpt\":[\"text\",\"FooBar\"]}}"
+
+  bytes_before_batch=$(${DOCXBOX_BINARY} txt "${PATH_DOCX_PLAINTEXT}" | wc --bytes)
+
+  run ${DOCXBOX_BINARY} batch "${PATH_DOCX_PLAINTEXT}" ${batch}
 
   check_for_valgrind_error
 
-  ${DOCXBOX_BINARY} txt ${PATH_DOCX} | grep --count "baz"
+  bytes_after_batch=$(${DOCXBOX_BINARY} txt ${PATH_DOCX} | wc --bytes)
+
+  (( bytes_before_batch > bytes_after_batch ))
 }
 
-@test "With \"${BASE_COMMAND} batch_sequence_as_JSON\" ${APPENDIX} and a string can be replaced by a h1" {
-  batch="{\"1\":{\"mm\":[\"title\",\"foo\"]},\"2\":{\"rpt\":[\"Officia\",\"baz\"]},\"3\":{\"rpt\":[\"Lorem\",{\"h1\":{\"text\":\"Quux\"}}]}}"
-  run ${DOCXBOX_BINARY} batch "${PATH_DOCX}" "${batch}"
+title_rmt="Case 5: With \"${BASE_COMMAND} batch_sequence_as_JSON\" ${APPENDIX} \
+and a string can be replaced with the \"rmt\" command"
+@test "${title_rmt}" {
+  batch="{\"1\":{\"rmt\":[\"THIS\",\"TITLE\"]}}"
+
+  wc_before_batch=$(${DOCXBOX_BINARY} txt "${PATH_DOCX_PLAINTEXT}" | wc --words)
+
+  run ${DOCXBOX_BINARY} batch "${PATH_DOCX_PLAINTEXT}" ${batch}
 
   check_for_valgrind_error
 
-  ${DOCXBOX_BINARY} cat "${PATH_DOCX}" "${display_file}" | grep --count "<w:pStyle w:val=\"para1\"/>"
+  wc_after_batch=$(${DOCXBOX_BINARY} txt "${PATH_DOCX_PLAINTEXT}" | wc --words)
+
+  (( wc_before_batch > wc_after_batch ))
 }
 
 check_for_valgrind_error() {
